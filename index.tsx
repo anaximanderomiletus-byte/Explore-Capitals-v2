581013aa92f7e6cc445210960b70b117e22bf98d
daniel <dcho2001@gmail.com>
2026-01-24 13:27:30 -0500

    Add minimal loading indicator inside #root (auto-replaced by React)

--- index.tsx
+++ index.tsx
@@ -2,22 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Hide loading screen once React starts mounting
-const hideLoader = () => {
-  const loader = document.getElementById('app-loader');
-  if (loader) {
-    loader.classList.add('fade-out');
-    setTimeout(() => loader.remove(), 300);
-  }
-};
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
-// Hide loader immediately when we start rendering
-hideLoader();
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
