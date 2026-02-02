df8ab273b53ba97809c14bbb9bcd3579fa03744d
daniel <dcho2001@gmail.com>
2026-02-02 14:36:28 -0500

    J

--- data/images/images_j.ts
+++ data/images/images_j.ts
@@ -1,25 +1,25 @@
 export const imagesJ: Record<string, string> = {
   // --- Jamaica --- (placeholder until images added)
-  "Jamaica": "/kingston-jamaica.png",
-  "Dunn's River Falls": "/dunns-river-falls.png",
+  "Jamaica": "/png/JAMAICA/kingston-jamaica.png",
+  "Dunn's River Falls": "/png/JAMAICA/dunns-river-falls.png",
   "Blue Mountains": "/blue-mountains.png",
-  "Bob Marley Museum": "/bob-marley-museum-56-hope-road.png",
-  "Rick's Café": "/ricks-cafe-negril-cliffs.png",
-  "Rose Hall Great House": "/rose-hall-great-house.png",
+  "Bob Marley Museum": "/png/JAMAICA/bob-marley-museum-56-hope-road.png",
+  "Rick's Café": "/png/JAMAICA/ricks-cafe-negril-cliffs.png",
+  "Rose Hall Great House": "/png/JAMAICA/rose-hall-great-house.png",
 
   // --- Japan --- (placeholder until images added)
-  "Japan": "/logo.png",
-  "Mount Fuji": "/logo.png",
-  "Shibuya Crossing": "/logo.png",
-  "Fushimi Inari Shrine": "/logo.png",
-  "Hiroshima Peace Memorial": "/logo.png",
-  "Himeji Castle": "/logo.png",
+  "Japan": "/png/JAPAN/tokyo-japan.png",
+  "Mount Fuji": "/png/JAPAN/mount-fuji.png",
+  "Shibuya Crossing": "/png/JAPAN/shibuya-crossing.png",
+  "Fushimi Inari Shrine": "/png/JAPAN/fushimi-inari-shrine.png",
+  "Hiroshima Peace Memorial": "/png/JAPAN/hiroshima-peace-memorial.png",
+  "Himeji Castle": "/png/JAPAN/himeji-castle.png",
 
   // --- Jordan --- (placeholder until images added)
-  "Jordan": "/logo.png",
-  "Petra Treasury": "/logo.png",
-  "Wadi Rum": "/logo.png",
-  "Dead Sea Jordan": "/logo.png",
-  "Jerash": "/logo.png",
-  "Mount Nebo": "/logo.png"
+  "Jordan": "/png/JORDAN/amman-jordan.png",
+  "Petra Treasury": "/png/JORDAN/petra-treasury.png",
+  "Wadi Rum": "/png/JORDAN/wadi-rum.png",
+  "Dead Sea Jordan": "/png/JORDAN/dead-sea-jordan.png",
+  "Jerash": "/png/JORDAN/jerash.png",
+  "Mount Nebo": "/png/JORDAN/mount-nebo.png"
 };
